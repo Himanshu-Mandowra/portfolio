e340a587cd62b65d7ee0cac5d147e73ef8d310ef
Himanshu Mandowra <himanshu.toppr@gmail.com>
2026-07-07 13:19:54 +0530

    jj

--- portfolio-backend/src/index.js
+++ portfolio-backend/src/index.js
@@ -1,25 +1,25 @@
 import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import dns from "node:dns";
 
-console.log("MONGO_URI:", process.env.MONGODB_URI)
+
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
-dotenv.config({
-    path:'./env'
-})
+console.log("MONGO_URI:", process.env.MONGODB_URI)
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port: ${process.env.PORT}`)
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`)
+        })
     })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!!! ", err)
-})
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!! ", err)
+    })
 
 
 
